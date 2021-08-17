5d61fc4a389de4451058503a58d44bd466b9fdfa
makoto <88492711+hihdrum@users.noreply.github.com>
2021-08-18 00:15:39 +0900

    ネタ追加

---
+++ numCharToInt.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int numChar2Int(char numChar)
+{
+  return 0;
+}
+int main(void)
+{
+  printf("%d", '0');
+  return 0;
+}
---
+++ prototype.c
@@ -0,0 +1,11 @@
+int f(void);
+
+int main(void)
+{
+  return f();
+}
+
+int f(void)
+{
+  return 1;
+}
