e9bf7834a8965bd2be3ea58a555bce4a8cc0acb1
makoto <88492711+hihdrum@users.noreply.github.com>
2021-08-19 00:59:25 +0900

    ネタ追加

--- helloworld.c
+++ helloworld.c
@@ -1,7 +1,7 @@
-#include <stdio.h>
+#include <stdio.h>            /* 最初はおまじない */
 
-int main(void)
+int main(void)                /* main関数 : おまじない */
 {
   printf("Hello, world!\n");
-  return 0;
+  return 0;                   /* 関数の戻り値 */
 }
