e363a45af605944dac23cc7ea7443d8d968fb408
makoto <88492711+hihdrum@users.noreply.github.com>
2021-11-20 00:37:14 +0900

    諸修正

--- array_int.c
+++ array_int.c
@@ -3,6 +3,7 @@
 int main(void)
 {
   int score[3] = { 10, 19, 17 };
+
   printf("score[%d] = %d\n", 0, score[0]);
   printf("score[%d] = %d\n", 1, score[1]);
   printf("score[%d] = %d\n", 2, score[2]);
